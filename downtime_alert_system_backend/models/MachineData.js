adb4172cdc2e79b28b197785c486596e9e935fde
Pamuditha Gangana <pamudithagangana45@gmail.com>
2025-08-05 23:43:43 +0530

    MQTT data save

--- downtime_alert_system_backend/models/MachineData.js
+++ downtime_alert_system_backend/models/MachineData.js
@@ -2,9 +2,13 @@ import mongoose from "mongoose";
 
 const machineDataSchema = new mongoose.Schema({
   machineId: String,
+  machineName: String, // ✅ added
+  machineType: String, // ✅ added
   status: String,
   temperature: Number,
   vibration: Number,
+  breakdownStartTime: Date, // ✅ added
+  breakdownEndTime: Date, // ✅ added
   timestamp: {
     type: Date,
     default: Date.now,
