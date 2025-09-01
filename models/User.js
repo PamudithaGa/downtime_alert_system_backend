19dfe75817c199a1b0797dffaba58273b555c771
Pamuditha Gangana <pamudithagangana45@gmail.com>
2025-09-01 23:39:32 +0530

    Update registration

--- models/User.js
+++ models/User.js
@@ -1,33 +1,63 @@
-//
 import mongoose from "mongoose";
 
 const lineSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  teamLeaders: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], 
+  teamLeaders: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 });
 
 const sectionSchema = new mongoose.Schema({
-  name: { type: String, required: true }, 
-  lines: [lineSchema], 
+  name: { type: String, required: true },
+  lines: [lineSchema],
 });
+
+const departmentRoles = {
+  production: [
+    "Team Leader",
+    "Group Leader",
+    "Value stream executive",
+    "Value stream manager",
+  ],
+  technical: ["Staff member", "Executive"],
+  engineering: ["Mechanic", "Executive"],
+  quality: ["Staff member", "Executive"],
+  cutting: ["Staff member", "Executive"],
+  industrialeng: ["Staff member", "Executive"],
+  subassembly: [
+    "Team Leader",
+    "Group Leader",
+    "Value stream executive",
+    "Head of Department",
+  ],
+};
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  epf: { type: String, required: true, unique: true },
   department: {
     type: String,
     required: true,
-    enum: ["technical", "quality", "industrialeng", "cutting", "subassembly", "production"],
+    enum: Object.keys(departmentRoles),
   },
-  createdAt: { type: Date, default: Date.now },
-  epf: { type: String, required: true, unique: true },
-  role: { type: String, required: true },
+  role: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return departmentRoles[this.department]?.includes(value);
+      },
+      message: (props) => `${props.value} is not a valid role for department `,
+    },
+  },
   production: {
     type: {
-      sections: [sectionSchema], 
+      sections: [sectionSchema],
     },
-    required: function () { return this.department === "production"; },
+    required: function () {
+      return this.department === "production";
+    },
   },
 });
 
